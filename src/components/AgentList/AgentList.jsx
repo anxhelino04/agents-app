3e0ce4f5aed3a6865044c3c38dda88bc7ed402e9
Anxhelin Cauli <0anxhelino@gmail.com>
2023-01-08 20:27:03 +0100

    onclick you can view agent details

--- src/components/AgentList/AgentList.jsx
+++ src/components/AgentList/AgentList.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import "./AgentList.css";
 const AgentList = ({ agents, deleteAgent, handleEdit, showEditModal }) => {
+  const [selectedID, setSelectedID] = useState();
+  console.log(selectedID, "selecetdid");
+
   return (
     <div className="container">
       <div className="cardDetails">
-        <h1>{agents[0]?.fname}</h1>
-        <p>{agents[0]?.pnumber}</p>
-        <p>{agents[0]?.email}</p>
-        <p>{agents[0]?.realestate}</p>
+        <h1>{agents[selectedID]?.fname || agents[0]?.fname}</h1>
+        <p>{agents[selectedID]?.pnumber || agents[0]?.pnumber}</p>
+        <p>{agents[selectedID]?.email || agents[0]?.email}</p>
+        <p>{agents[selectedID]?.realestate || agents[0]?.realestate}</p>
       </div>
       <div style={{ flex: 1 }}>
         {agents?.map((agent, index) => {
           return (
             <div className="agentCard" key={index}>
               <div style={{ marginInline: "6.5em" }}>
-                <p className="fname">{agent?.fname}</p>
+                <p
+                  onClick={() => {
+                    setSelectedID(index);
+                  }}
+                  className="fname"
+                >
+                  {agent?.fname}
+                </p>
                 <p className="content">{agent?.pnumber}</p>
                 <p className="content">{agent?.email}</p>
                 <p className="content">{agent?.realestate}</p>
